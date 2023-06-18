da1eae7b67a36585ee2893d693407857832531e8
Aldi Dharmawan <108856819+Chexillon@users.noreply.github.com>
2023-06-18 22:45:34 +0700

    Update about.blade.php
    Update caption and image in about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,21 +1,20 @@
 <x-app-layout>
   <div class="about container">
     <div class="row">
-      <div class="col">
-        <img class="rounded" src="https://i.pinimg.com/564x/d8/20/d7/d820d7bcb9b5f9bf3012b5d062d64a9f.jpg" alt="">
+      <div class="col mb-5">
+        <img class="rounded w-100" src="https://i.pinimg.com/564x/d8/20/d7/d820d7bcb9b5f9bf3012b5d062d64a9f.jpg" alt="">
       </div>
-      <div class="col">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus doloribus unde asperiores rem, explicabo vitae suscipit neque ipsa illo aspernatur odit qui, eveniet provident eius expedita incidunt veritatis deserunt recusandae! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis natus similique iure iste odio illo, magnam maxime nesciunt corrupti officia ipsum ab, earum beatae reiciendis eos exercitationem est, aut pariatur?</p>
+      <div class="col text-justify">
+        <p>Kualitas jalan yang terjamin, kesejahteraan masyarakat terangkat! Temukan kemudahan melaporkan jalanan rusak di jalanmulus.id, website yang peduli akan kenyamanan dan keselamatan Anda. Bersama-sama, kita wujudkan perbaikan infrastruktur yang lebih baik untuk masa depan yang lebih lancar!</p>
       </div>
     </div>
 
     <div class="row">
       <div class="col">
-        <img class="rounded mt-5" src="https://i.pinimg.com/564x/d8/20/d7/d820d7bcb9b5f9bf3012b5d062d64a9f.jpg" alt="">
+        <img class="rounded mt-5 w-100" src="https://i.pinimg.com/564x/2f/be/9e/2fbe9e94bd1e65a850b3dd4d103491b6.jpg" alt="">
       </div>
-      <div class="col mt-5">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, sapiente sed nihil iusto, illo accusantium voluptatem suscipit nesciunt tempore nemo dolorem dignissimos aperiam enim blanditiis voluptatibus quia maxime odio! Quo. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis natus similique iure iste odio illo, magnam maxime nesciunt corrupti officia ipsum ab, earum beatae reiciendis eos exercitationem est, aut pariatur?</p>
-
+      <div class="col mt-5 text-justify">
+      <p>2023, Era Baru Perbaikan Jalan! Sambut kehadiran jalanmulus.id, website inovatif yang hadir untuk mensejahterakan penduduk dengan mempermudah melaporkan jalanan rusak. Bersama-sama, kita membangun masa depan yang lebih baik dengan infrastruktur yang aman dan nyaman untuk semua.</p>
       </div>
     </div>
   </div>
