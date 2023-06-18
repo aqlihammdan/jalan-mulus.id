56e5fdb344882f04a37aaf52e34377a15500bb92
Aldi Dharmawan <108856819+Chexillon@users.noreply.github.com>
2023-06-18 23:31:48 +0700

    Update about.blade.php
    Update Tittle and caption

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,11 +1,13 @@
 <x-app-layout>
   <div class="about container">
-    <div class="row">
+    <h1 class="fw-bold mb-3 fs-4 text-center">TENTANG KAMI</h1>
+    <hr>
+    <div class="row mt-5">
       <div class="col mb-5">
         <img class="rounded w-100" src="https://i.pinimg.com/564x/d8/20/d7/d820d7bcb9b5f9bf3012b5d062d64a9f.jpg" alt="">
       </div>
-      <div class="col text-justify">
-        <p>Kualitas jalan yang terjamin, kesejahteraan masyarakat terangkat! Temukan kemudahan melaporkan jalanan rusak di jalanmulus.id, website yang peduli akan kenyamanan dan keselamatan Anda. Bersama-sama, kita wujudkan perbaikan infrastruktur yang lebih baik untuk masa depan yang lebih lancar!</p>
+      <div class="col text-justify lh-lg">
+        <p>Kami adalah jalanmulus.id, sebuah website yang berdedikasi untuk kesejahteraan penduduk dengan fokus pada perbaikan jalan. Dengan semangat yang kuat, kami hadir untuk membantu masyarakat melaporkan jalanan yang rusak dan berkontribusi dalam memperbaiki infrastruktur kota. Dengan kerjasama dari masyarakat, kami bertekad untuk menciptakan jalanan yang aman, nyaman, dan bebas dari kerusakan. Bergabunglah dengan kami, bersama-sama kita menjadi agen perubahan untuk kualitas hidup yang lebih baik.</p>
       </div>
     </div>
 
@@ -13,8 +15,8 @@
       <div class="col">
         <img class="rounded mt-5 w-100" src="https://i.pinimg.com/564x/2f/be/9e/2fbe9e94bd1e65a850b3dd4d103491b6.jpg" alt="">
       </div>
-      <div class="col mt-5 text-justify">
-      <p>2023, Era Baru Perbaikan Jalan! Sambut kehadiran jalanmulus.id, website inovatif yang hadir untuk mensejahterakan penduduk dengan mempermudah melaporkan jalanan rusak. Bersama-sama, kita membangun masa depan yang lebih baik dengan infrastruktur yang aman dan nyaman untuk semua.</p>
+      <div class="col mt-5 text-justify lh-lg">
+        <p>Website kesejahteraan penduduk yang lahir pada tahun 2023 dengan misi yang jelas: membantu masyarakat melaporkan jalanan yang rusak. Dengan semangat inovasi dan kepedulian terhadap infrastruktur yang berkualitas, kami berkomitmen untuk memperbaiki kualitas hidup masyarakat melalui perbaikan jalanan. Dengan platform kami yang mudah digunakan, kami memberikan wadah bagi masyarakat untuk berpartisipasi aktif dalam melaporkan kerusakan jalan dan menjadi bagian dari solusi. Bersama-sama, mari kita bangun kota yang lebih baik, di mana setiap langkah di atas jalanan terasa mulus dan aman bagi semua.</p>
       </div>
     </div>
   </div>
